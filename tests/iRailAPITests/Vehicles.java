d909351fc63d23b6ef58e4d35fe61c833ed02302
Pieter Colpaert <pieter@irail.be>
2010-11-20 02:44:31 +0100

    added JUnittests

---
+++ tests/iRailAPITests/Vehicles.java
@@ -0,0 +1,41 @@
+package iRailAPITests;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author pieterc
+ */
+public class Vehicles {
+
+    public Vehicles() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+
+}
